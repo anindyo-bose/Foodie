bad2e2a10a4bd6d8e0c58895990ce91a6ab5c92a
Anindyo Bose <boseanindyo05@gmail.com>
2017-08-20 01:23:23 +0530

    Tutorial 3 Completed

--- main.js
+++ main.js
@@ -6,3 +6,7 @@
 //       $('.welcome-message').text(message);
 //   });
 // });
+var foodieApp = angular.module('foodieApp',[]);
+foodieApp.controller('mainController',function($scope) {
+	$scope.restaurants = ['Farzi Cafe','Pizza Hut','Wenger\'s Deli','Sagar Ratna'];
+})
